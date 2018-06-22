90e6eee80dbec00568184eac61088a66c2441434
Yossef <yuanfu.yang@zhixingglobal.com>
2018-06-22 22:50:16 +0800

    add koa-router

--- server/index.js
+++ server/index.js
@@ -4,13 +4,20 @@ const mongoose = require('mongoose');
 const views = require('koa-views');
 const { resolve } = require('path');
 const { connect, initSchemas } = require('./database/init');
+const router = require('./routes');
+
 
 ;(async () => {
     await connect();
     initSchemas();
     // require('./tasks/movie')
-    require('./tasks/api')
+    // require('./tasks/api')
 })()
+
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
 
 app.use(views(resolve(__dirname, './views'), {
     extension: 'pug'
